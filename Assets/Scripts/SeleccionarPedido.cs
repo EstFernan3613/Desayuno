860aa18d1cc8c8a0f4d23005097ff3620b65541d
EstFernan3613 <juan_est.fernandez@uao.edu.co>
2023-08-22 19:42:22 -0500

    Cambios y Empiezo de Escenas y Pedidos
    Weh

--- Assets/Scripts/SeleccionarPedido.cs
+++ Assets/Scripts/SeleccionarPedido.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EfectoSonido : MonoBehaviour
+public class SeleccionarPedido : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
